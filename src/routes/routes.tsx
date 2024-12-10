35c96197b2f9a203a6350a0c9e1d6f330864a866
AlexandreDresch <connectWithAlexandre@proton.me>
2024-12-09 23:34:59 -0300

    chore: initial project setup with essential dependencies

---
+++ src/routes/routes.tsx
@@ -0,0 +1,13 @@
+import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
+
+import Home from "@/pages/home";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route element={<Home />} path="/" />
+      </Switch>
+    </BrowserRouter>
+  );
+}
