1a8d41a2d08bb700eaacf0686a5dc0923b9290ce
Júlia Bueno Seganfredo <150600044+jb-seganfredo@users.noreply.github.com>
2024-12-13 21:39:29 -0300

    fix: fixig design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,52 @@
 import { Separator } from "@/components/ui/separator";
+import { Ghost, MapPin, Mail, Dot } from "lucide-react";
 
 export default function Footer() {
   return (
-    <footer className="flex w-full flex-col items-center justify-center bg-gray-800">
-      <span className="flex p-2">
-        <img src="/githubIcon.svg" alt="GitHub" />
-        <img src="/instagram.svg" alt="Instagram" />
-        <img src="/linkedInIcon.svg" alt="LinkedIn" />
-      </span>
+    <footer className="flex w-full flex-col items-center justify-center bg-slate-50 dark:bg-black">
+      <div className="flex flex-col py-5 lg:flex-row lg:gap-32">
+        <div className="flex flex-col items-center gap-2">
+          <div className="flex items-center justify-center text-lg">
+            <Ghost />
+            <h2>StellioCode</h2>
+          </div>
+          <p className="px-4 text-sm">Amazing Software House</p>
+          <span className="flex p-2">
+            <img
+              src="/githubIcon.svg"
+              alt="GitHub"
+              className="zoom-in-50 hover:animate-in"
+            />
+            <img src="/instagram.svg" alt="Instagram" />
+            <img src="/linkedInIcon.svg" alt="LinkedIn" />
+          </span>
+        </div>
 
+        <div className="my-1 flex flex-col items-center justify-center gap-4 text-sm lg:py-0">
+          <div className="flex gap-2">
+            <a href="#">Sobre</a>
+            <Dot />
+            <a href="#">Porfólio</a>
+            <Dot />
+            <a href="#">Planos</a>
+            <Dot />
+            <a href="#">Entre em contato</a>
+          </div>
+          <div>
+            <p className="flex items-center justify-center gap-2 p-2">
+              <MapPin /> Porto Alegre - RS
+            </p>
+            <p className="flex gap-2 p-2">
+              <Mail /> software@stelliocode.com
+            </p>
+          </div>
+        </div>
+      </div>
       <Separator
         orientation="horizontal"
         className="w-11/12 bg-slate-500 opacity-45"
       />
-      <p className="p-2 text-gray-300">
+      <p className="p-2 text-xs text-gray-500">
         © 2024 StellioCode. All rights reserved.
       </p>
     </footer>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,33 +1,107 @@
 import { Button } from "./ui/button";
+import { motion } from "framer-motion";
+
+// interface SectionProps {
+//   children: React.ReactNode;
+// }
+
+// function Section({ children }: SectionProps) {
+//   const ref = useRef(null);
+//   const isInView = useInView(ref, { once: true });
+
+//   return (
+//     <section ref={ref}>
+//       <span
+//         style={{
+//           transform: isInView ? "none" : "translateX(-200px)",
+//           opacity: isInView ? 1 : 0,
+//           transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+//         }}
+//       >
+//         {children}
+//       </span>
+//     </section>
+//   );
+// }
+
+const AnimatedText = ({ text }: { text: string }) => {
+  return (
+    <span>
+      {text.split("").map((char, index) => (
+        <motion.span
+          key={index}
+          style={{ display: "inline-block" }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{
+            delay: index * 0.15,
+            duration: 0.5,
+          }}
+        >
+          {char}
+        </motion.span>
+      ))}
+    </span>
+  );
+};
+
+// const FadeInText = ({ text }: { text: string }) => {
+//   return (
+//     <motion.div
+//       initial={{ opacity: 0 }}
+//       animate={{ opacity: 1 }}
+//       transition={{
+//         duration: 2,
+//         ease: "easeOut",
+//       }}
+//     >
+//       {text}
+//     </motion.div>
+//   );
+// };
 
 export default function Hero() {
   return (
-    <div className="flex min-h-lvh flex-col items-center justify-center">
-      <main className="flex gap-20">
-        <div className="flex flex-col items-center justify-center">
-          <h1 className="text-6xl">
-            Bem-vindo a{" "}
-            <div className="bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text text-transparent">
-              StellioCode
-            </div>
-          </h1>
-          <p className="mt-4 p-4 text-4xl text-gray-700">
+    <div className="my-10 flex min-h-screen flex-col items-center justify-center md:h-lvh xl:min-h-lvh">
+      <main className="flex items-center justify-center xl:gap-24">
+        <div className="m-4 flex flex-col justify-center gap-5">
+          {/* <Section> */}
+          <div className="text-5xl lg:text-7xl lg:font-semibold">
+            {/* <FadeInText text="Bem-vindo a" /> */}
+            <h2>Bem-vindo a</h2>
+          </div>
+          <div className="animate-gradient bg-[linear-gradient(to_right,theme(colors.indigo.400),theme(colors.indigo.100),theme(colors.sky.400),theme(colors.fuchsia.400),theme(colors.sky.400),theme(colors.indigo.100),theme(colors.indigo.400))] bg-[length:200%_auto] bg-clip-text text-6xl font-semibold text-transparent">
+            <AnimatedText text="StellioCode" />
+          </div>
+
+          <p className="mt-4 p-4 text-left text-3xl text-slate-500 xl:text-4xl">
             Onde sonhos viram websites!
           </p>
-          <Button
-            variant="secondary"
-            className="text-1xl bg-gradient-to-r from-pink-500 to-purple-600 text-white"
-            size="lg"
-          >
-            Marque sua reunião!
-          </Button>
+          {/* </Section> */}
+
+          <div className="flex justify-center py-4">
+            <motion.div
+              whileHover={{
+                scale: 1.1,
+                transition: { duration: 0.2 },
+              }}
+            >
+              <Button
+                variant="secondary"
+                className="text-1xl bg-gradient-to-r from-sky-500 to-fuchsia-400 text-white"
+                size="lg"
+              >
+                Marque sua reunião!
+              </Button>
+            </motion.div>
+          </div>
         </div>
 
         <div>
           <img
             src="/heroImage.png"
             alt="Hero"
-            className="object-fit h-96 w-full"
+            className="object-fit hidden h-96 w-full xl:flex"
           />
         </div>
       </main>
