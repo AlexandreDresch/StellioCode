0160f25c5debb86c82010109d98bf65dba308a2f
AlexandreDresch <connectWithAlexandre@proton.me>
2024-12-17 00:04:36 -0300

    feat: add initial dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,20 @@
+import AdminDashboard from "@/components/dashboard/admin-dashboard";
+import DevDashboard from "@/components/dashboard/dev-dashboard";
+import Sidebar from "@/components/dashboard/sidebar";
+
+interface DashboardProps {
+  userRole: string;
+}
+
+export default function Dashboard({ userRole }: DashboardProps) {
+  if (userRole !== "admin" && userRole !== "dev") {
+    return <h1>Unauthorized Access</h1>;
+  }
+
+  return (
+    <div>
+      <Sidebar />
+      {userRole === "admin" ? <AdminDashboard /> : <DevDashboard />}
+    </div>
+  );
+}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,26 @@
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 
 import Home from "@/pages/home";
+import ProtectedRoute from "@/components/protected-route";
+import Dashboard from "@/pages/dashboard";
 
 export default function Routes() {
+  const user = { role: "admin" };
+
   return (
     <BrowserRouter>
       <Switch>
         <Route element={<Home />} path="/" />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute
+              isAllowed={user.role === "admin" || user.role === "dev"}
+            >
+              <Dashboard userRole={user.role} />
+            </ProtectedRoute>
+          }
+        />
       </Switch>
     </BrowserRouter>
   );
