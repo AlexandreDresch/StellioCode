4c2cca74f1ff13424d8a86b1d2e88b090db430ed
AlexandreDresch <connectWithAlexandre@proton.me>
2025-01-12 00:02:26 -0300

    feat: add id & currentProjectId properties to developers list

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,16 @@
-export interface IDeveloperCardProps {
+export interface IDeveloper {
+  id: string;
   name: string;
   email: string;
   avatarUrl: string;
   role: string;
+  currentProjectId: string | null;
   projectsCount: number;
   githubUrl: string;
   techStack: string[];
+}
+
+export interface IDeveloperCardProps extends IDeveloper {
+  isSelected: boolean;
+  onToggleSelect: (id: string) => void;
 }
