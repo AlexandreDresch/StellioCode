40b6766ca82d3efcd239c6863a9da30cbfdcd22e
AlexandreDresch <connectWithAlexandre@proton.me>
2025-01-11 23:59:35 -0300

    refactor: moved projectsData constant to constants folder

--- src/components/dashboard/admin-dashboard.tsx
+++ src/components/dashboard/admin-dashboard.tsx
@@ -5,70 +5,16 @@ import { columns } from "./table/columns";
 import { ColumnDef } from "@tanstack/react-table";
 import AreaChartGradient from "./charts/area-chart-gradient";
 import BarChartComponent from "./charts/bar-chart";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { projectsData } from "@/constants/projects";
 
 export default function AdminDashboard() {
-  
-
-  const projectsData = [
-    {
-      id: "1",
-      title: "Redesign de Website",
-      client: "TechCorp Inc.",
-      price: 1234.56,
-      status: "in_progress",
-    },
-    {
-      id: "2",
-      title: "Desenvolvimento de App Mobile",
-      client: "HealthPlus",
-      price: 2274.56,
-      status: "pending",
-    },
-    {
-      id: "3",
-      title: "Campanha de Marketing",
-      client: "GreenFields",
-      price: 3034.5,
-      status: "completed",
-    },
-    {
-      id: "4",
-      title: "Plataforma de E-commerce",
-      client: "ShopEasy",
-      price: 1400.0,
-      status: "cancelled",
-    },
-    {
-      id: "5",
-      title: "Migração para a Nuvem",
-      client: "DataSecure",
-      price: 1234.56,
-      status: "in_progress",
-    },
-    {
-      id: "6",
-      title: "Estratégia para Mídias Sociais",
-      client: "CreativeSolutions",
-      price: 1000.56,
-      status: "pending",
-    },
-    {
-      id: "7",
-      title: "Auditoria de Cibersegurança",
-      client: "SecureNet",
-      price: 10034.56,
-      status: "completed",
-    },
-    {
-      id: "8",
-      title: "Desenvolvimento de Chatbot com IA",
-      client: "SmartTalk",
-      price: 4234.56,
-      status: "in_progress",
-    },
-  ];
-
   return (
     <div className="p-4 sm:ml-14">
       <section className="flex gap-4">
